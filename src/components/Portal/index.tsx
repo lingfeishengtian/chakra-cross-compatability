a6aeb67bee9c937bf689c518b0e5e74552c5810e
Hunter Han <hunter.han@koddi.com>
2025-07-25 09:32:30 -0500

    Add repository to package

--- src/components/Portal/index.tsx
+++ src/components/Portal/index.tsx
@@ -1,12 +1,15 @@
-import type { PortalProps as ChakraPortalProps } from "@chakra-ui/react";
-import { Portal } from "@chakra-ui/react";
-import type { PropsWithChildren } from "react";
+import type { PortalProps as ChakraPortalProps } from '@chakra-ui/react';
+import { Portal } from '@chakra-ui/react';
+import type { PropsWithChildren } from 'react';
 
 export type PortalProps = PropsWithChildren<ChakraPortalProps>;
 
+/**
+ * Warning: This component uses react-portals in Chakra v2 which may cause issues with framer-motion animations.
+ */
 const ChakraCompatiblePortal = (props: PortalProps) => {
-  const { children, ...rest } = props;
-  return <Portal {...rest}>{children}</Portal>;
-}
+    const { children, ...rest } = props;
+    return <Portal {...rest}>{children}</Portal>;
+};
 
 export default ChakraCompatiblePortal;
